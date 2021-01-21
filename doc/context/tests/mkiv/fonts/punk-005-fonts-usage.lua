c2f202063c104ffb2baa17ed1e402862e82d5356
Adam Reviczky <adam.reviczky@kclalumni.net>
2021-01-21 23:07:00 +0000

    latest 2021-01-18 18:58

--- doc/context/tests/mkiv/fonts/punk-005-fonts-usage.lua
+++ doc/context/tests/mkiv/fonts/punk-005-fonts-usage.lua
@@ -1,2 +1,38 @@
 return {
+ {
+  ["filename"]="c:/data/develop/tex-context/tex/texmf/fonts/opentype/public/lm-math/latinmodern-math.otf",
+  ["instances"]=3,
+  ["size"]=733500,
+  ["version"]="Version 1.958",
+ },
+ {
+  ["filename"]="c:/data/develop/tex-context/tex/texmf/fonts/opentype/public/lm/lmmono10-regular.otf",
+  ["instances"]=1,
+  ["size"]=64684,
+  ["version"]="Version 2.004;PS 2.004;hotconv 1.0.49;makeotf.lib2.0.14853",
+ },
+ {
+  ["filename"]="c:/data/develop/tex-context/tex/texmf/fonts/opentype/public/lm/lmroman10-bold.otf",
+  ["instances"]=1,
+  ["size"]=111240,
+  ["version"]="Version 2.004;PS 2.004;hotconv 1.0.49;makeotf.lib2.0.14853",
+ },
+ {
+  ["filename"]="c:/data/develop/tex-context/tex/texmf/fonts/opentype/public/lm/lmroman10-regular.otf",
+  ["instances"]=1,
+  ["size"]=111536,
+  ["version"]="Version 2.004;PS 2.004;hotconv 1.0.49;makeotf.lib2.0.14853",
+ },
+ {
+  ["filename"]="c:/data/develop/tex-context/tex/texmf/fonts/opentype/public/lm/lmromanslant10-bold.otf",
+  ["instances"]=1,
+  ["size"]=117984,
+  ["version"]="Version 2.004;PS 2.004;hotconv 1.0.49;makeotf.lib2.0.14853",
+ },
+ {
+  ["filename"]="c:/data/develop/tex-context/tex/texmf/fonts/opentype/public/lm/lmromanslant10-regular.otf",
+  ["instances"]=1,
+  ["size"]=118508,
+  ["version"]="Version 2.004;PS 2.004;hotconv 1.0.49;makeotf.lib2.0.14853",
+ },
 }
